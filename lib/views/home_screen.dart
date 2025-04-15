a43cc9d3382a8e75eed565d113fb1fbd99d47938
Zianur <mdzianurjoy@gmail.com>
2025-04-16 02:28:19 +0600

    feature: bottom navigation with tabbar done now need to need to add noteview screen and add note screen

--- lib/views/home_screen.dart
+++ lib/views/home_screen.dart
@@ -2,12 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:unicap_cg/controllers/auth_controller.dart';
 import 'package:unicap_cg/controllers/diary_controller.dart';
-import 'package:unicap_cg/controllers/fav_caption_controller.dart';
-import 'package:unicap_cg/services/auth_service.dart';
-import 'package:unicap_cg/views/caption_category.dart';
+import 'package:unicap_cg/views/caption_category_screen.dart';
 import 'package:unicap_cg/views/fav_caption_screen.dart';
 import '../controllers/caption_category_controller.dart';
-import 'caption_screen.dart';
 import 'diary_screen.dart';
 import 'login_screen.dart';
 
@@ -16,68 +13,67 @@ class HomeScreen extends StatefulWidget {
   _HomeScreenState createState() => _HomeScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen> {
-  int _selectedIndex = 0;
-  final PageController _pageController = PageController();
+class _HomeScreenState extends State<HomeScreen> with SingleTickerProviderStateMixin {
+  late TabController _tabController;
 
   @override
   void initState() {
     super.initState();
+
+    _tabController = TabController(length: 4, vsync: this);
+
     Provider.of<CaptionCategoryController>(context, listen: false).loadCategories();
+
+    final authController = Provider.of<AuthController>(context, listen: false);
+    if (authController.isLoggedIn) {
+      String? userId = authController.user?.uid;
+      Provider.of<DiaryController>(context, listen: false).fetchAndSaveNotes(userId ?? '');
+    }
   }
 
-  // List of screens for navigation
-  final List<Widget> _screens = [
-    DiaryScreen(),
-    CaptionCategoryView(),
-    LoginScreen(),
-    FavCaptionScreen(),
-  ];
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: PageView(
-        controller: _pageController,
-        children: [_screens[_selectedIndex]],
-        onPageChanged: (index) {
-          setState(() {
-            _selectedIndex = index;
-          });
-        },
-      ), // Display selected screen
-
-      // ✅ Bottom Navigation Bar
-      bottomNavigationBar: BottomNavigationBar(
-        selectedItemColor: Theme.of(context).primaryColor,
-        unselectedItemColor: Colors.black,
-        currentIndex: _selectedIndex,
-        onTap: (index) {
-          setState(() {
-            _selectedIndex = index;
-          });
-        },
-        items: [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.book),
-            label: 'Diary',
-          ),
-
-          BottomNavigationBarItem(
-            icon: Icon(Icons.category),
-            label: 'Captions',
-          ),
-
-          BottomNavigationBarItem(
-            icon: Icon(Icons.login),
-            label: 'Login',
-          ),
-
-          BottomNavigationBarItem(
-            icon: Icon(Icons.favorite),
-            label: 'Favorite',
-          ),
+      body: TabBarView(
+        controller: _tabController,
+        children: [
+          DiaryScreen(),
+          CaptionCategoryView(),
+          FavCaptionScreen(),
+          LoginScreen(),
         ],
+      ),
+      bottomNavigationBar: Container(
+        height: 60,
+        decoration: BoxDecoration(
+          color: Theme.of(context).primaryColor,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black12,
+              blurRadius: 10,
+              offset: Offset(0, -2),
+            ),
+          ],
+        ),
+        child: TabBar(
+          controller: _tabController,
+          labelColor: Colors.white,
+          unselectedLabelColor: Colors.black,
+          indicatorColor: Colors.transparent,
+          dividerColor: Colors.transparent,
+          tabs: [
+            Tab(icon: Icon(Icons.category), text: 'Captions'),
+            Tab(icon: Icon(Icons.book), text: 'Diary'),
+            Tab(icon: Icon(Icons.favorite), text: 'Favorite'),
+            Tab(icon: Icon(Icons.login), text: 'Login'),
+          ],
+        ),
       ),
     );
   }
