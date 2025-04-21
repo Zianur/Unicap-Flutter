b49641fc2159e7f7421f5aa5d7b85d2b8a274a2c
Zianur <mdzianurjoy@gmail.com>
2025-04-22 01:49:46 +0600

    code standard

--- lib/views/login_screen.dart
+++ lib/views/login_screen.dart
@@ -25,48 +25,47 @@ class _LoginScreenState extends State<LoginScreen> {
     final double heightSize = MediaQuery.sizeOf(context).width;
 
     return Consumer<AuthController>(
-      builder: (context, authController, _) {
-        return Scaffold(
-          backgroundColor: Colors.deepPurpleAccent,
-          body: Center(
-            child: SingleChildScrollView(child: Column(children: [
+        builder: (context, authController, _) {
+          return Scaffold(
+            backgroundColor: Colors.deepPurpleAccent,
+            body: Center(
+              child: SingleChildScrollView(child: Column(children: [
 
-              authController.user != null ? Column(children: [
-                /// profile Section
-                Container(
-                  width: widthSize,
-                  padding: EdgeInsets.all(20),
-                  color: Colors.deepPurpleAccent,
-                  child: Column(children: [
+                authController.user != null ? Column(children: [
+                  /// profile Section
+                  Container(
+                    width: widthSize,
+                    padding: EdgeInsets.all(20),
+                    color: Colors.deepPurpleAccent,
+                    child: Column(children: [
 
-                    Icon(Icons.person, size: 200),
-                    SizedBox(width: 20),
+                      Icon(Icons.person, size: 200),
+                      SizedBox(width: 20),
 
-                    Container(
-                      alignment: Alignment.center,
-                      width: widthSize,
-                      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(5),
-                        border: Border.all(width: 1, color: Colors.white),
+                      Container(
+                        alignment: Alignment.center,
+                        width: widthSize,
+                        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(5),
+                          border: Border.all(width: 1, color: Colors.white),
+                        ),
+                        child: Text(authController.user?.email ?? '', style: TextStyle(fontSize: 18, color: Colors.white)),
                       ),
-                      child: Text(authController.user?.email ?? '', style: TextStyle(fontSize: 18, color: Colors.white)),
-                    ),
-                  ]),
-                ),
-                // SizedBox(height: heightSize * 0.125),
-
-                Padding(
-                  padding: const EdgeInsets.all(20),
-                  child: CustomButtonWidget(
-                    buttonText: 'Logout',
-                    onPressed: ()=> authController.signOut(),
-                    backgroundColor: Colors.red.withValues(alpha: 0.85),
+                    ]),
                   ),
-                ),
+                  // SizedBox(height: heightSize * 0.125),
 
-              ]) : Center(
-                child: CustomButtonWidget(
+                  Padding(
+                    padding: const EdgeInsets.all(20),
+                    child: CustomButtonWidget(
+                      buttonText: 'Logout',
+                      onPressed: ()=> authController.signOut(),
+                      backgroundColor: Colors.red.withValues(alpha: 0.85),
+                    ),
+                  ),
+
+                ]) : Center(child: CustomButtonWidget(
                   margin: 20,
                   onPressed: () async {
                     await authController.signInWithGoogle();
@@ -76,19 +75,18 @@ class _LoginScreenState extends State<LoginScreen> {
                   backgroundColor: Colors.white,
                   textStyle: TextStyle(color: Colors.black),
                   icon: Container(
-                      margin: EdgeInsets.symmetric(horizontal: 10),
-                      height: 30,
-                      width: 30,
-                      child: Image.asset('assets/png/google_icon.png', fit: BoxFit.cover),
+                    margin: EdgeInsets.symmetric(horizontal: 10),
+                    height: 30,
+                    width: 30,
+                    child: Image.asset('assets/png/google_icon.png', fit: BoxFit.cover),
                   ),
                   textColor: Colors.black,
-                ),
-              ),
+                )),
 
-            ])),
-          ),
-        );
-      }
+              ])),
+            ),
+          );
+        }
     );
   }
 }
